ba8737b2bdb7714b5068ac8d7abf4ca0793c8c39
PasinduLakshan99 <lakshanpasindu685@gmail.com>
2025-03-22 17:25:41 +0530

    restore gui cursor upon exiting nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:ver1
+    augroup END
+]])
